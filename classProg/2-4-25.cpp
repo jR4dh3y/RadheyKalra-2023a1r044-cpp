3a9a4913f28ff652d1171788acca367b4be5651b
jR4dh3y <radheykalra901@gmail.com>
2025-04-03 15:24:52 +0530

    .

--- classProg/2-4-25.cpp
+++ classProg/2-4-25.cpp
@@ -73,11 +73,56 @@ int main(){
 
 SINGLE level inhertace 
 
-write a program to 
-a. create a base class with name myapp
-b. create a fn with name wellcome_message() , accept pin from user and calidate pin against value 123, other function will execute only if pin is valid
-d. 
+Example;- Single level inheritance
+Write a program to
+a. Create a base class with name MyAPP
+b. Create a function with name Welcome_Message(), to display welcome message on Screen
+c. Create a function with Lgin(), accept pin from user and validate pin against value 123, other function will execute only after successful login of user
+d. Create a function Accept(), store student information like, registation number, marks
+e. Create a function Display() to display information entered by the student
+f. Create a derived class with name Grade_Cal
+g. Create a function Calc(), reads marks from MyApp class and function Cal() to calculate percentage and grades as per following:0 
+Percentage between 80 to 100 A grade, 70 to 80 B grade, 60 to 70 C grade other wise not qualitifed.
+
 */
 
+class myApp{
+    private:
+        void welcome_message(){
+            cout << "Welcome to MyApp" << endl;
+        }
+        int login(){
+            int pin;
+            cout << "Enter pin: ";
+            cin >> pin;
+            return (pin == 123) ? 1 : 0;
+        }
+    public:
+        myApp( ){
+            if(login()){
+                cout << "Login successful" << endl;
+                welcome_message();
+            } else {
+                cout << "Login failed"<< endl;
+                return;
+            }
+        }
+        void accept(){
+            int reg_no, marks;
+            cout << "Enter registration number: ";
+            cin >> reg_no;
+            cout << "Enter marks: ";
+            cin >> marks;
+        }
 
+        void display(){}
+};
 
+class grade_cal : public myApp{};
+
+int main(){
+    myApp app;
+    app.accept();
+    app.display();
+    return 0;
+}
